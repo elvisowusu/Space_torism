25bdaab8d826133b03e161e48e97c43c4ce1f1e2
elvisowusu <owusuelvisgyasi@gmail.com>
2024-05-13 13:53:08 +0000

    refactor: Update Home and NavItems components with responsive design and font styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,19 +7,19 @@ function Home() {
     }
     return (
         <div className=" flex flex-col mt-[2rem] items-center">
-            <section className="text-center h-[276px] w-[327px]">
-                <h2 className="text-[#D0D6F9] tracking-[2.7px] font-barlowCondensed font-normal text-[1rem]">
+            <section className="text-center h-[276px] w-[327px] md:h-full md:w-[400px]">
+                <h2 className="text-[#D0D6F9] tracking-[2.7px] md:tracking-[3.38px] font-barlowCondensed font-normal text-[1rem] md:text-[16px]">
                     SO, YOU WANT TO TRAVEL TO 
                 </h2>
-                <h1 className="text-white font-bellefair text-[80px] mt-1 mb-5">
+                <h1 className="text-white font-bellefair text-[80px] md:text-[110px] md:leading-[150px] mt-1 mb-5">
                     SPACE
                 </h1>
-                <p className="text-[#D0D6F9] leading-[25px] text-[15px] font-barlow">
+                <p className="text-[#D0D6F9] leading-[25px] md:leading-[28px] text-[15px] md:text-[15px] font-barlow">
                     Let&apos;s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we&apos;ll give you a truly out of this world experience!
                 </p>
             </section>
-            <div className="group/item relative flex justify-center items-center  mt-[7rem] mb-[2.6rem]">
-                <button onClick={handleClick} className={`group/edit ${clicked?'animate-ping':''} rounded-full z-20 font-bellefair tracking-[1.25px] text-[20px] h-[150px] w-[150px] bg-white text-[#0B0D17]`}>
+            <div className="group/item relative flex justify-center items-center  mt-[7rem] md:mt-[10rem] mb-[2.6rem] md:mb-0">
+                <button onClick={handleClick} className={`md:absolute group/edit ${clicked?'animate-ping':''} rounded-full z-20 font-bellefair tracking-[1.25px] md:tracking-[1.5px] text-[20px] md:text-[30px] h-[150px] md:h-[180px] w-[150px] md:w-[180px] bg-white text-[#0B0D17]`}>
                     EXPLORE
                 </button>
                 <div className={`group-hover/item:visible group-hover/item:animate-ping invisible absolute z-10 opacity-30 bg-[#979797] w-[12.5rem] h-[12.5rem] rounded-full`}></div>
--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -7,7 +7,7 @@ function NavItems() {
         <nav className="h-full md:bg-[#979797] lg:pl-[1rem] lg:pr-[5rem] lg:z-10">
             <ul className='flex flex-col justify-evenly md:items-center md:flex-row gap-[2rem] text-[#FFFFFF] font-barlowCondensed md:gap-[1rem] lg:gap-[2rem] md:w-[35rem] md:opacity-70 md:h-[100%] lg:w-[830px]'>
                 {items.map((item, index) => (
-                    <li key={index} className='cursor-pointer hover:underline underline-offset-[1.75rem] tracking-[2.7px]'>
+                    <li key={index} className='cursor-pointer hover:underline underline-offset-[1.75rem] tracking-[2.7px] md:tracking-[2.36px] md:test-[14px]'>
                         <span className="md:hidden lg:inline-flex font-bold  mr-2">{index.toString().padStart(2, '0')}</span>{item}
                     </li>
                 ))}
