bea4f4336d25f716fb64b689ffb14c6126b924f2
elvisowusu <owusuelvisgyasi@gmail.com>
2024-05-13 00:24:41 +0000

    refactor: Update Header component with responsive menu and state management

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -2,12 +2,14 @@
 
 
 function NavItems() {
-    const items = ['00 HOME', '01 DESTINATION', '02 CREW', '03 TECHNOLOGY'];
+    const items = [' HOME', ' DESTINATION', ' CREW', ' TECHNOLOGY'];
     return (
         <nav>
-            <ul className='flex flex-col justify-evenly md:flex-row lg:gap-[2rem] md:gap-[1rem]  lg:w-[830px]'>
+            <ul className='flex flex-col justify-evenly md:flex-row gap-[2rem] text-[#FFFFFF] md:gap-[1rem] lg:gap-[2rem] md:w-[720px] lg:w-[830px]'>
                 {items.map((item, index) => (
-                    <li key={index} className=''>{item}</li>
+                    <li key={index} className=''>
+                        <span className="md:hidden font-bold tracking-widest mr-2 ">{index.toString().padStart(2, '0')}</span>{item}
+                    </li>
                 ))}
             </ul>
         </nav>
