a4d6d1708f2c14fffab96444ab1e14d1e9134448
elvisowusu <owusuelvisgyasi@gmail.com>
2024-05-13 09:34:01 +0000

    navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,17 +32,9 @@ function Header() {
     
     
     return (
-        <header className='relative flex items-center justify-between h-[4.5rem] pl-[1.5rem] pr-[1.5rem] md:pr-0 '>
+        <header className='relative flex items-center justify-between h-[4.5rem] pl-[1.5rem] lg:mt-[2rem] lg:ml-[3rem]  pr-[1.5rem]  md:pr-0 '>
             <button><img src={logo} height={40} alt="" /></button>
-                {/* {state.screenWidth> 768?
-                <div className='flex'>
-                    <hr className='text-red-700 w-[473px] md:w-0'/>
-                    <NavItems/>
-                </div>:
-                    <button className='z-20' onClick={() => dispatch({type: "SET_CLICKED", payload: !state.clicked})}><img src={!state.clicked? menuBar:close} alt="" /></button>
-                } */}
-            
-                <hr className='text-red-700 lg:w-[473px] hidden lg:block'/>
+                <hr className='absolute left-[7rem] border-none h-[0.2rem] opacity-30 bg-[#979797] z-20 lg:w-[473px] hidden lg:block'/>
                 <div className='hidden md:block h-[100%]'><NavItems/></div>
                 <button className='z-20 md:hidden' onClick={() => dispatch({type: "SET_CLICKED", payload: !state.clicked})}><img src={!state.clicked? menuBar:close} alt="" /></button>
             {state.clicked?
--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -4,11 +4,11 @@
 function NavItems() {
     const items = [' HOME', ' DESTINATION', ' CREW', ' TECHNOLOGY'];
     return (
-        <nav className="h-full md:bg-[#979797]">
-            <ul className='flex flex-col justify-evenly items-center md:flex-row gap-[2rem] text-[#FFFFFF] md:gap-[1rem] lg:gap-[2rem] md:w-[35rem] md:opacity-70 md:h-[100%] lg:w-[830px]'>
+        <nav className="h-full md:bg-[#979797] opacity-45 lg:pl-[1rem] lg:pr-[5rem] lg:z-10">
+            <ul className='flex flex-col justify-evenly md:items-center md:flex-row gap-[2rem] text-[#FFFFFF] md:gap-[1rem] lg:gap-[2rem] md:w-[35rem] md:opacity-70 md:h-[100%] lg:w-[830px]'>
                 {items.map((item, index) => (
                     <li key={index} className='cursor-pointer hover:underline underline-offset-[1.75rem]'>
-                        <span className="md:hidden font-bold tracking-widest mr-2 ">{index.toString().padStart(2, '0')}</span>{item}
+                        <span className="md:hidden lg:inline-flex font-bold tracking-widest mr-2 ">{index.toString().padStart(2, '0')}</span>{item}
                     </li>
                 ))}
             </ul>
