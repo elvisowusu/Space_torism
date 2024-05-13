16e772d054b6362717ba89a77c5e20654ec53e0e
elvisowusu <owusuelvisgyasi@gmail.com>
2024-05-13 10:30:36 +0000

    chore: Update App.jsx and Home.jsx components with responsive design and background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
+import Home from "./components/Home"
 
 
 function App() {
 
   return (
-    <div className="">
+    <div className="h-[100vh]">
       <Header/>
+      <Home/>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,22 @@
 
+
 function Home() {
     return (
-        <div>
-            
+        <div className="bg-[url(src\assets\homeBackground.jpg)] flex flex-col items-center ">
+            <section className="text-center h-[276px] w-[327px]">
+                <h2>
+                    SO, YOU WANT TO TRAVEL TO 
+                </h2>
+                <h1 >
+                    SPACE
+                </h1>
+                <p className="text-[#D0D6F9]">
+                    Let&apos;s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we&apos;ll give you a truly out of this world experience!
+                </p>
+            </section>
+            <button className="rounded h-[150px] w-[150px]">
+                EXPLORE
+            </button>
         </div>
     );
 }
