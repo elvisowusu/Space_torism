9083a91d074d06c37c7d4aec56d70ee42b4504cc
elvisowusu <owusuelvisgyasi@gmail.com>
2024-05-15 13:17:57 +0000

    more features

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./components/Home"
 function App() {
 
   return (
-    <div className={`bg-[url(src/assets/homeBackground.jpg)] bg-no-repeat md:bg-repeat-round lg:bg-no-repeat bg-cover md:bg-contain  overflow-hidden h-screen lg:h-[100vh]`}>
+    <div className={`bg-[url(src/assets/homeBackground.jpg)] lg:bg-[url(src/assets/lghomeBackground.jpg)] bg-no-repeat md:bg-repeat-round lg:bg-no-repeat bg-cover md:bg-contain  lg:bg-cover overflow-hidden h-screen lg:h-[100vh]`}>
       <Header/>
       <Home/>
     </div>
